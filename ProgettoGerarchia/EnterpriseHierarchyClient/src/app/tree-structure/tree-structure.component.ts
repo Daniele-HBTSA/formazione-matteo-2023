3ea25f5dd57d5333f9622ebca811d12eb6c0c67c
Ecelon9 <ecelon93@gmail.com>
2023-06-29 12:52:44 +0200

    Prima struttura albero funzionante

--- ProgettoGerarchia/EnterpriseHierarchyClient/src/app/tree-structure/tree-structure.component.ts
+++ ProgettoGerarchia/EnterpriseHierarchyClient/src/app/tree-structure/tree-structure.component.ts
@@ -7,20 +7,20 @@ import { NestedTreeControl } from '@angular/cdk/tree';
 @Component({
   selector: 'app-tree-structure',
   templateUrl: './tree-structure.component.html',
-  styleUrls: ['./tree-structure.component.css']
+  styleUrls: ['./tree-structure.component.css'],
 })
 export class TreeStructureComponent implements OnInit{
 
-  nestedDataSource = new MatTreeNestedDataSource<EnterprieseTree>();
-  nestedTreeControl = new NestedTreeControl<EnterprieseTree>(node => node.Children);
+  dataSource = new MatTreeNestedDataSource<EnterprieseTree>();
+  treeControl = new NestedTreeControl<EnterprieseTree>(node => node.Children);
 
-  constructor(private treeStruct : TreeService) { }
+  constructor(private treeStruct: TreeService) { }
 
   ngOnInit(): void {
     this.treeStruct.GetTreeFromDB().subscribe({
       next: (tree: EnterprieseTree[]) => {
         if (tree != null) {
-          this.nestedDataSource.data = tree;
+          this.dataSource.data = tree;
           console.log(tree)
         }
       },
@@ -30,7 +30,5 @@ export class TreeStructureComponent implements OnInit{
     });
   };
 
-  hasChildren(index: number, node: EnterprieseTree): boolean {
-    return node?.Children?.length > 0;
-  }
+  hasChild = (_: number, node: EnterprieseTree) => !!node.Children && node.Children.length > 0;
 }
